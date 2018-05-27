09b5b8369e08af79b85d0806980ceae418ce9a8b
devnacho <nachojgutierrez@gmail.com>
2018-05-27 08:57:41 -0300

    styles waiting page

--- bouncer-app/src/app/app.component.ts
+++ bouncer-app/src/app/app.component.ts
@@ -27,8 +27,7 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Home', component: HomePage }
     ];
 
   }
