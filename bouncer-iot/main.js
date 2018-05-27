cc6409747b8a64bb95a77e1123370193517ac6d5
chelo <chelo_c@live.com>
2018-05-27 00:09:44 -0300

    fullscreen

--- bouncer-iot/main.js
+++ bouncer-iot/main.js
@@ -105,6 +105,7 @@ function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
+  mainWindow.setFullScreen(true);
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
 
