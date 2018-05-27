05effdf837e0ff9074d4468277bbf0f82fe200bb
chelo <chelo_c@live.com>
2018-05-27 07:47:08 -0300

    cleanup

--- bouncer-iot/providers/accessService.js
+++ bouncer-iot/providers/accessService.js
@@ -3,12 +3,6 @@
 var myWeb3= require('../providers/myWeb3');
 
 
-
-    // function checkAccess(address incomingPerson) public view returns (bool) {
-    //     return accessAllowance[incomingPerson];
-    // }
-
-
 exports.checkAccess= function(addr){
   return new Promise( (resolve, reject) =>{
     myWeb3.myContractInstance.checkAccess(addr)
@@ -16,9 +10,6 @@ exports.checkAccess= function(addr){
       console.log('result from checkAddress',result);
       resolve(result);
 
-      // let ethStr=myWeb3.web3.fromWei(result['blue'],'ether');
-      // let ethStr2=myWeb3.web3.fromWei(result['red'],'ether');
-      // resolve([ethStr,ethStr2]);
     })
     .catch((err) => {
       console.log(err);
